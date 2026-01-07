78e09ae76c6f6622db7788ef4cb396f269aba5ca
Prakhar Bhatt <149253252+BhattPrakhar@users.noreply.github.com>
2026-01-07 16:46:32 +0000

    Comaprison again

---
+++ basics/comparison.js
@@ -0,0 +1,23 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2<1)
+// console.log(2!=1)
+// console.log(2==1)
+
+
+console.log("2">1)
+console.log("0 2">1)
+
+console.log(null > 0)
+console.log(null >= 0)
+console.log(null == 0)
+ 
+
+// in js == and (<,>,>=,<==) works differently == means null is 0 so it returns false, but in comparison opeartor converts null to a number so that it returns TRUE
+// === checks values as well as its datatypes strictly
+
+
+
+
+
+  

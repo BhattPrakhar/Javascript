684b6d3c6536fdb70a3c9dd4f6d8e74e2d7f659b
Prakhar Bhatt <149253252+BhattPrakhar@users.noreply.github.com>
2026-01-07 15:42:25 +0000

    Conversion and Operations

---
+++ basics/conversionOperation.js
@@ -0,0 +1,56 @@
+// /*************** CONVERSIONS **************/
+// let score = "33abe";
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log (valueInNumber) // NaN => not a number
+
+// // "33"=> 33
+// // " 33abs"=> NaN
+// // true => 1; false=> 0
+// // null => 0; undefined = NaN 
+
+
+// let isLoggedIn = 1;
+// let booleanisLoggedIn = Boolean (isLoggedIn)
+// console.log(booleanisLoggedIn)
+
+// // 1 => true; 0=>false
+// // " " => false
+// // "prakhar"=> true
+
+
+
+// let someNumber =33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+// console.log(someNumber)
+
+
+/********** OPERATIONS **********/
+
+
+
+let value = 3;
+let negValue = -value
+// console.log(negValue);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log((0+0) + 2 * 3 % 3);
+// console.log(true);
+// console.log(+true)
+// console.log(-"");
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter)
+
+

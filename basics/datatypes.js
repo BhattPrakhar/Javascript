e1381790e8964b56ebfcb4f870fd62baa16ca965
Prakhar Bhatt <149253252+BhattPrakhar@users.noreply.github.com>
2026-01-07 13:49:56 +0000

    Datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,26 @@
+"Use Strict"; // treat all js code as newer version
+
+// alert("hello") we are using nodejs, not browser
+
+let name ="prakhar"
+let age =3;
+let isLoggedIn = true
+let State = null
+
+//number => 2 to power 53  
+// bigint
+// string =" "
+//boolean = true /false
+// null is a standalone value            
+// undefined = means no value is defined to it 
+//symbol = for uniqueness
+
+
+
+// object 
+
+console.log(typeof"prakhar");
+console.log(typeof"null");
+console.log(typeof null); //output is => object
+console.log(typeof undefined); // output is => undefined
+
